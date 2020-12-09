282ff975723b0a1b068f6fae19024a43b3e813ba
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-12-09 13:01:46 +0100

    practised with one-way and two-ways bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-// for dynamics in the template
+
+  // for dynamics in the template
   title = 'Dynamic title'
   number = 42
   arr = [1, 2, 3]
-// objects are not supported as they are; angular tries to make it a string
+  // objects are not supported as they are; angular tries to make it a string
+  img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFw9kPoQAbytNDNL6a-L7wvif1LELEcXDx9g&usqp=CAU'
+
+  inputValue = ''
+
+  title2 = 'Initial'
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Timeout is over!');
+      this.img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRREaUDlF9m7Ymx5fo8q56K3A7viWEG25y2Rw&usqp=CAU'
+    }, 5000)
+  }
+
+  onInput (event: KeyboardEvent) {
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+
+  onClick () {
+    console.log("Click!");
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str;
+  }
+
+  onInput2(event: any) {
+    this.title2 = event.target.value;
+  }
 }
