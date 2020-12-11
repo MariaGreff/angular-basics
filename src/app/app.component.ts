1f82a37ce3887d44e86398d47c9d1eb9ab2d07ed
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-12-11 16:38:37 +0100

    practised with passing of parameters to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Interface } from 'readline';
+
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
@@ -7,5 +14,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  showPlayground = false;
-
+ posts: Post[] = [
+   {title: 'Want to learn Angular', text: 'I am still learning components', id: 1},
+   {title: 'Next block', text: 'Will be about directives and pipes', id: 2},
+   {title: 'One more block', text: 'Will be about something interesting', id: 3}
+ ]
 }
